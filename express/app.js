2e52c9e5ffcaaf17e66dcea118d26ce042e25c32
yfcai1227 <yfcai1227@gmail.com>
2018-03-25 11:32:34 -0400

    about change

--- express/app.js
+++ express/app.js
@@ -4,7 +4,7 @@ app.set('view engine','ejs');
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/',function(req,res){
-  res.send("hello");
+  res.render('about_me');
 })
 
 app.get('/about_me',function(req,res){
@@ -13,6 +13,12 @@ app.get('/about_me',function(req,res){
   res.render('about_me',{user:user});
 })
 
+
+app.get('/about',function(req,res){
+//     res.send(req.query.name);
+    var user = {name:req.query.name }
+  res.render('about',{user:user});
+})
 
 app.listen(3000,function(){
   console.log("start server on port 3000！");
