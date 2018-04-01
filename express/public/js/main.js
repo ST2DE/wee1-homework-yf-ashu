8c4b75f10dffa2ef1701d2ec4a65732aaf1e9bf5
yfcai1227 <yfcai1227@gmail.com>
2018-04-01 15:46:20 -0400

    about update

--- express/public/js/main.js
+++ express/public/js/main.js
@@ -1,29 +1,26 @@
 window.onload = function () {
-    document.body.className += 'loaded'
-  }
-  var toggle = document.querySelector('.toggle')
-  var target = document.querySelector('.popup--menu')
-  var close = document.querySelector('.toggle--close')
-  toggle.addEventListener('click', toggleChange, false)
-  
-  function toggleChange (e) {
-    toggle.style.display = 'none'
-    target.style.visibility = 'visible'
-    target.style.opacity = '1'
-    target.style.right = '0'
-    setTimeout(function () {
-      close.style.display = 'block'
-      close.style.visibility = 'visible'
-      close.style.opacity = '1'
-    }, 450)
-  }
-  close.addEventListener('click', closeChange, false)
-  function closeChange (e) {
-    toggle.style.display = 'block'
-    target.style.visibility = 'hidden'
-    target.style.opacity = '0'
-    target.style.right = '-100%'
-    close.style.display = 'none'
-    close.style.visibility = 'hidden'
-    close.style.opacity = '0'
-  }
+  document.body.className += 'loaded'
+  document.querySelector('.bg').style.height = window.innerHeight + "px";
+  timer();
+}
+window.onresize = function () {
+  document.querySelector('.bg').style.height = window.innerHeight + "px";
+}
+const toggle = document.querySelector('.toggle');
+const target = document.querySelector('.popup--menu');
+const close = document.querySelector('.toggle--close');
+
+function timer() {
+  const timer = setTimeout(function () {
+    document.querySelector(".bg").classList.remove("pre");
+  }, 1000);
+}
+
+function toggleChange() {
+  toggle.classList.toggle("toggle--hidden");
+  close.classList.toggle('toggle--close--active');
+  target.classList.toggle('popup--menu--active');
+}
+
+toggle.addEventListener('click', toggleChange, false);
+close.addEventListener('click', toggleChange, false);
